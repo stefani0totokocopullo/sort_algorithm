7b29b02789a630d83d2af10985dbec3bc3bfa6eb
stefani7 <stefani.toto@gmail.com>
2019-01-14 05:36:51 -0500

    message 1

---
+++ main.cpp
@@ -0,0 +1,321 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+
+struct node
+  {
+    int data;
+    node *next,*head, *tail;
+  };
+
+
+
+
+void display(vector <int> &vec, int n){
+
+ cout << "The vector elements are: \n";
+    for (int i = 0; i < vec.size(); i++){
+        cout << vec[i] << " "<<endl;}
+
+}
+/* Selection sort O(n^2) */
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+void selectionSort(vector <int> &vec, int n)
+{
+    int i, j, min_idx =vec[0];
+
+    // One by one move boundary of unsorted subarray
+    for (i = 0; i < vec.size()-1; i++)
+    {
+        // Find the minimum element in unsorted array
+        for (j = i+1; j < vec.size(); j++) {
+          if (vec[j] < vec[min_idx])
+            min_idx = j;
+            cout<<"vec \t"<<vec[j]<<"\t"<<vec[min_idx]<<endl;
+        // Swap the found minimum element with the first element
+            swap(vec[min_idx], vec[i]);}
+    }
+}
+
+
+/* Bubble sort O(n^2) */
+void bubbleSort(vector <int> &vec, int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)
+
+       // Last i elements are already in place
+       for (j = 0; j < n-i-1; j++)
+           if (vec[j] > vec[j+1])
+             // swap(&arr[j], &arr[j+1]);
+             vec[0]=1;
+}
+
+
+/*  insertion sort O(n^2) */
+void insertionSort(int arr[], int n)
+{
+   int i, key, j;
+   for (i = 1; i < n; i++)
+   {
+       key = arr[i];
+       j = i-1;
+
+       /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+       while (j >= 0 && arr[j] > key)
+       {
+           arr[j+1] = arr[j];
+           j = j-1;
+       }
+       arr[j+1] = key;
+   }
+}
+
+
+/* Merge sort O(n logn ) */
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    /* create temp arrays */
+    int L[n1], R[n2];
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    /* Copy the remaining elements of L[], if there
+       are any */
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    /* Copy the remaining elements of R[], if there
+       are any */
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+/* l is for left index and r is right index of the
+   sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l+(r-l)/2;
+
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+
+/* quick sort O(n^2) */
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+    array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+int partition (int arr[], int low, int high)
+{
+    int pivot = arr[high];    // pivot
+    int i = (low - 1);  // Index of smaller element
+
+    for (int j = low; j <= high- 1; j++)
+    {
+        // If current element is smaller than or
+        // equal to pivot
+        if (arr[j] <= pivot)
+        {
+            i++;    // increment index of smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+ arr[] --> Array to be sorted,
+  low  --> Starting index,
+  high  --> Ending index */
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        /* pi is partitioning index, arr[p] is now
+           at right place */
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+
+/* Heap sort O(n logn) */
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = 2*i + 1; // left = 2*i + 1
+    int r = 2*i + 2; // right = 2*i + 2
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+// main function to do heap sort
+void heapSort(int arr[], int n)
+{
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i=n-1; i>=0; i--)
+    {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+
+int main()
+{
+
+
+    node *next = NULL;
+    next->head = NULL;
+    //next->tail = 12;
+
+    vector <int> vec;
+    cout<<"Enter number elemenets \t "<<endl;
+	int n;
+	cin >> n;
+    int nr;
+	//vec.assign(n,0);
+
+
+/*
+        for (int i = 1; i <= n; i++) {
+        cout<<"Element ["<<i<<"] \n";
+        cin>>nr;
+        vec.push_back(nr); }
+
+    cout << "vector length is \t"<<vec.size()<<endl;
+    display(vec,n);
+
+    char choose ;
+
+    cout << "Select the desired algorithm \n"
+          <<"1: selectionSort \n"
+          <<"2: bubbleSort \n"
+          <<"3: insertionSort \n"
+          <<"4: merge \n"
+          <<"5: mergeSort \n"
+          <<"6: quickSort \n"
+          <<"7: heapify \n"
+          <<"8: heapSort \n";
+
+     cin >> choose;
+
+     switch(choose) {
+     case '1': cout <<" selectionSort \n";
+     selectionSort(vec,n);
+     break;
+
+     case 2: cout <<" bubbleSort \n";
+     break;
+
+     case 3: cout <<" insertionSort \n";
+     break;
+
+     case 4: cout <<" merge \n";
+     break;
+
+     case 5: cout <<" mergeSort \n";
+     break;
+
+      case 6: cout <<" quickSort \n";
+     break;
+
+      case 7: cout <<" heapify \n";
+     break;
+
+     case 8: cout <<" heapSort \n";
+     break;
+
+     default: cout << "choose again ";
+     break;
+}
+
+    //insertelement();
+  display(vec,n);
+*/
+
+	return 0;
+
+}
